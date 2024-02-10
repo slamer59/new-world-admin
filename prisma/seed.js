48b61ab271302a31f0d4b3a0a1122198a238b614
Pedot T <thomas.pedot@jokosun.com>
2024-02-10 18:23:43 +0100

    Update PlayerType schema in dbml and prisma

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,15 +4,16 @@ import {
   PrismaClient,
   RoleType,
   Rune,
+  WarPosition,
   Weapon,
   WeightLimit,
 } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const numberOfPlayer = 20
-function getRandomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+const numberOfPlayer = 20;
+const numberOfPlayerType = 15
+const numberOfWarComposition = 10
+
 
 async function seed() {
   try {
@@ -38,13 +39,19 @@ async function seed() {
       WeightLimit.Medium,
       WeightLimit.Heavy,
     ];
-    console.log("🚀 ~ seed ~ weightList:", weightList)
+    
     const runeList = [
       Rune.Detonate,
-      Rune.Fire_Storm,
-      Rune.Grasping_Vines,
       Rune.Stoneform,
+      Rune.FireStorm,
+      Rune.GraspingVines,
+      Rune.BileBomb,
+      Rune.CanonBlast,
+      Rune.DarkAscent,
+      Rune.PrimalFury,
+      Rune.TheDevourer,
     ];
+    
     const weaponList = [
       Weapon.Bow,
       Weapon.FireStaff,
@@ -59,13 +66,17 @@ async function seed() {
       Weapon.Spear,
       Weapon.Swords,
       Weapon.WarHammer,
-      Weapon.VoidGauntlet
+      Weapon.VoidGauntlet,
     ];
-    console.log("🚀 ~ seed ~ weaponList:", weaponList)
+
+    const positionList = [
+      WarPosition.top_left, WarPosition.top_right, WarPosition.bottom_left, WarPosition.bottom_right,
+      WarPosition.point, WarPosition.flank, WarPosition.flex
+    ]
 
     // Seed additional users
     for (let i = 0; i < numberOfPlayer; i++) {
-      await prisma.user.upsert({
+      const user = await prisma.user.upsert({
         where: { id: i },
         update: {},
         create: {
@@ -74,37 +85,70 @@ async function seed() {
           email: faker.internet.email(),
         },
       });
-      // await prisma.role.upsert({
-      //   where: { id: i },
-      //   update: {},
-      //   create: {
-      //     id: i,
-      //     role: getRandomFromArray(roleList),
-      //     rune: getRandomFromArray(runeList),
-      //     weapon: getRandomFromArray(weaponList),
-      //     weightLimit: getRandomFromArray(weightList),
-      //   },
-        
-      // });
-    }
-    // Seed the Player table
-    // await prisma.player.upsert({
-    //   where: { id: 1 },
-    //   update: {},
-    //   create: {
-    //     id: 1,
-    //     user_id: 1,
-    //     character_name: faker.name.findName(),
-    //     level: faker.datatype.number(),
-    //     faction: faker.random.arrayElement(['Marauders', 'Syndicate', 'Covenant']) as Faction,
-    //     role: [faker.random.arrayElement(['Bruiser', 'Disrupter', 'Heal', 'Mage', 'Ranged', 'Support', 'Tank', 'zzz_lvling'])] as Role[],
-    //     weight: faker.random.arrayElement(['Light', 'Medium', 'Heavy']) as Weight,
-    //     rune_coeur: faker.random.arrayElement(['Detonate', 'Stoneform', 'Fire_Storm', 'Grasping_Vines']) as Rune_Coeur,
-    //     spec: faker.random.arrayElement(['GA_WH', 'SnS_GS', 'GS_HA', 'GS_Spear', 'SnS_Spear', 'Spear_HA', 'GA_BB', 'Healer_DST', 'Healer_Clap']) as Spec,
-    //     status_id: 1,
-    //   },
-    // });
 
+      // Defined Roles
+      const role = await prisma.role.upsert({
+        where: { id: i },
+        update: {},
+        create: {
+          id: i,
+          role: faker.helpers.arrayElement(roleList),
+          rune: faker.helpers.arrayElement(runeList),
+          weapon: faker.helpers.arrayElement(weaponList),
+          weightLimit: faker.helpers.arrayElement(weightList),
+        },
+      });
+
+      // Seed the Player table
+      await prisma.player.upsert({
+        where: { id: i },
+        update: {},
+        create: {
+          name: faker.person.firstName(),
+          user: {
+            connect: { id: user.id },
+          },
+          roles: {
+            connect: { id: role.id },
+          },
+          status: {
+            create: {
+              ticket: faker.helpers.arrayElement([true, false]),
+              discord: faker.helpers.arrayElement([true, false]),
+              gear_check: faker.helpers.arrayElement([true, false]),
+              pov: faker.helpers.arrayElement([true, false]),
+              status_ticket: faker.lorem.sentence(3),
+            },
+          },
+        },
+      });
+    }
+
+    
+    function generatePlayers(roleList, levelList, count) {
+      const players = [];
+      for (let i = 0; i < count; i++) {
+        players.push({
+          name: faker.person.firstName(),
+          role: faker.helpers.arrayElement(roleList),
+          level: faker.helpers.arrayElement(levelList),
+        });
+      }
+      return players;
+    }
+    // Create group war Composition
+    for (let i = 0; i < numberOfWarComposition; i++) {
+    await prisma.warComposition.create({
+      data: {
+        // name: faker.helpers.fromRegExp('Group #[1-10]'),
+        name: `Group # ${i}`,
+        position: faker.helpers.arrayElement(positionList),
+        playerType: {
+          create: generatePlayers(roleList, levelList, numberOfPlayerType),
+        },
+      },
+    })
+  }
     console.log("Database seeded successfully!");
   } catch (error) {
     console.error("Error seeding database:", error);
